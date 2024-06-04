a24f8b20b6be820050e4f6e2aa8311be2e1d7f24
Pratik Waghmare <pratik.waghmare@criodo.com>
2024-06-04 12:00:00 +0530

    debug: env issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { env } from "./config/env.ts";
 
-createRoot(document.getElementById('root')!).render(
+if (env.VITE_DEBUG) {
+  console.log("RUNNINING IN DEBUG MODE");
+}
+
+console.log(env.VITE_BACKEND_URL); // ✅ guaranteed to be a valid URL
+console.log(env.VITE_DEBUG);       // ✅ boolean
+console.log(env.VITE_ENVIRONMENT); // ✅ safe enum value
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
